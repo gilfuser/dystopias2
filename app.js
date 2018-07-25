82aa8379145d753292f28eca62c2981d88edb6d8
Gil Fuser <gilfuser@gmail.com>
2018-07-25 00:08:07 -0300

    express serve static

--- app.js
+++ app.js
@@ -11,17 +11,12 @@ try {
   }
 }
 
-// Initialise DataChannel.js
-var datachannel = new DataChannel();
-
-// Set the userid based on what has been defined by DataChannel
-// https://github.com/muaz-khan/WebRTC-Experiment/tree/master/DataChannel#use-custom-user-ids
-datachannel.userid = window.userid;
-
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Cibele, meu amor!"))
+// app.get('/', (req, res) => res.send("Cibele, meu amor!"))
+
+app.use(express.static('public'))
 
 app.listen(5000, () => console.log('Server started on port 5000.'))
