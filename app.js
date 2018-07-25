04396f1a4234dc63a702752c897cc0eaf20b280d
Gil Fuser <gilfuser@gmail.com>
2018-07-25 14:59:19 -0300

    working version

--- app.js
+++ app.js
@@ -11,12 +11,61 @@ try {
   }
 }
 
+// -------------------------------------------------------------
+// SET UP PUSHER
+// -------------------------------------------------------------
+const Pusher = require("pusher");
+const pusher = new Pusher({
+  appId: config.app_id,
+  key: config.key,
+  secret: config.secret,
+  cluster: config.cluster
+});
+
+// ------------------------------------------------------
+// SET UP EXPRESS
+// ------------------------------------------------------
+
+
 const express = require('express')
-
+const bodyParser = require("body-parser");
+const errorHandler = require("errorhandler");
 const app = express()
+const root = __dirname + "/public";
 
-// app.get('/', (req, res) => res.send("Cibele, meu amor!"))
+// Parse application/json and application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-app.use(express.static('public'))
+// Simple logger
+app.use( (req, res, next) => {
+  console.log("%s %s", req.method, req.url);
+  console.log(req.body);
+  next();
+});
 
-app.listen(5000, () => console.log('Server started on port 5000.'))
+// Error handler
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}))
+
+// Serve static files from directory
+app.use(express.static(root));
+
+// Message proxy
+app.post("/message", (req, res) => {
+  let socketId = req.body.socketId;
+  let channel = req.body.channel
+  let message = req.body.message
+
+  pusher.trigger(channel, "message", message, socketId)
+
+  res.send(200)
+})
+
+// Open server on specified port
+const port = process.env.PORT || 5000;
+app.listen( port, () => console.log('Server started on port:', port))
