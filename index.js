28319e1d7963dec3628855d2acd246d03df48614
Gil Fuser <gilfuser@gmail.com>
2018-07-24 23:24:19 -0300

    initial commit

--- index.js
+++ index.js
@@ -13,12 +13,8 @@ try {
 
 const express = require('express')
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => {
-    re.send("Cibele, meu amor!")
-})
+app.get('/', (req, res) => res.send("Cibele, meu amor!"))
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000.')
-})
+app.listen(5000, () => console.log('Server started on port 5000.'))
