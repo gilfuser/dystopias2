906893ad73f742b00459ee7778944ab79462c220
Gil Fuser <gilfuser@gmail.com>
2018-07-25 22:50:22 -0300

    fake OSC msging

--- public/chat-osc.js
+++ public/chat-osc.js
@@ -1,24 +1,24 @@
 // Initialise DataChannel.js
-var datachannel = new DataChannel();
+const datachannel = new DataChannel();
 
 // Set the userid based on what has been defined by DataChannel
 // https://github.com/muaz-khan/WebRTC-Experiment/tree/master/DataChannel#use-custom-user-ids
 datachannel.userid = window.userid;
 
 // Open a connection to Pusher
-var pusher = new Pusher("72bd0eccc241c799a018", { cluster: "us2" });
+const pusher = new Pusher("72bd0eccc241c799a018", { cluster: "us2" });
 
 // Storage of Pusher connection socket ID
 var socketId;
 
-Pusher.log = function(message) {
+Pusher.log = (message) => {
   if (window.console && window.console.log) {
     window.console.log(message);
   }
 };
 
 // Monitor Pusher connection state
-pusher.connection.bind("state_change", function(states) {
+pusher.connection.bind("state_change", (states) => {
   switch (states.current) {
     case "connected":
       socketId = pusher.connection.socket_id;
@@ -32,11 +32,11 @@ pusher.connection.bind("state_change", function(states) {
 
 // Set custom Pusher signalling channel
 // https://github.com/muaz-khan/WebRTC-Experiment/blob/master/Signaling.md
-datachannel.openSignalingChannel = function(config) {
-  var channel = config.channel || this.channel || "default-channel";
-  var xhrErrorCount = 0;
+datachannel.openSignalingChannel = (config) => {
+  let channel = config.channel || this.channel || "default-channel";
+  let xhrErrorCount = 0;
 
-  var socket = {
+  let socket = {
     send: function(message) {
       $.ajax({
         type: "POST",
@@ -48,7 +48,7 @@ datachannel.openSignalingChannel = function(config) {
           message: message
         },
         timeout: 1000,
-        success: function(data) {
+        success: (data) => {
           xhrErrorCount = 0;
         },
         error: function(xhr, type) {
@@ -67,7 +67,7 @@ datachannel.openSignalingChannel = function(config) {
   };
 
   // Subscribe to Pusher signalling channel
-  var pusherChannel = pusher.subscribe(channel);
+  let pusherChannel = pusher.subscribe(channel);
 
   // Call callback on successful connection to Pusher signalling channel
   pusherChannel.bind("pusher:subscription_succeeded", function() {
@@ -82,8 +82,8 @@ datachannel.openSignalingChannel = function(config) {
   return socket;
 };
 
-var onCreateChannel = function() {
-  var channelName = cleanChannelName(channelInput.value);
+const onCreateChannel = function() {
+  let channelName = cleanChannelName(channelInput.value);
 
   if (!channelName) {
     console.log("No channel name given");
@@ -95,8 +95,8 @@ var onCreateChannel = function() {
   datachannel.open(channelName);
 };
 
-var onJoinChannel = function() {
-  var channelName = cleanChannelName(channelInput.value);
+const onJoinChannel = function() {
+  let channelName = cleanChannelName(channelInput.value);
 
   if (!channelName) {
     console.log("No channel name given");
@@ -109,44 +109,41 @@ var onJoinChannel = function() {
   datachannel.connect(channelName);
 };
 
-var cleanChannelName = function(channel) {
+const cleanChannelName = (channel) => {
   return channel.replace(/(\W)+/g, "-").toLowerCase();
 };
 
-var onSendMessage = function() {
-  var message = messageInput.value;
-
+const onSendMessage = () => {
+  let message = messageInput.value;
   if (!message) {
     console.log("No message given");
     return;
   }
-
   datachannel.send(message);
   addMessage(message, window.userid, true);
-
   messageInput.value = "";
 };
 
-var onMessageKeyDown = function(event) {
+const onMessageKeyDown = (event) => {
   if (event.keyCode == 13){
     onSendMessage();
   }
 };
 
-var addMessage = function(message, userId, self) {
-  var messages = messageList.getElementsByClassName("list-group-item");
+const addMessage = (message, userId, self) => {
+  let messages = messageList.getElementsByClassName("list-group-item");
 
   // Check for any messages that need to be removed
-  var messageCount = messages.length;
-  for (var i = 0; i < messageCount; i++) {
-    var msg = messages[i];
+  let messageCount = messages.length;
+  for (let i = 0; i < messageCount; i++) {
+    let msg = messages[i];
 
     if (msg.dataset.remove === "true") {
       messageList.removeChild(msg);
     }
   };
 
-  var newMessage = document.createElement("li");
+  let newMessage = document.createElement("li");
   newMessage.classList.add("list-group-item");
 
   if (self) {
@@ -159,33 +156,70 @@ var addMessage = function(message, userId, self) {
   messageList.appendChild(newMessage);
 };
 
-var disableConnectInput = function() {
+const disableConnectInput = function() {
   channelInput.disabled = true;
   createChannelBtn.disabled = true;
   joinChannelBtn.disabled = true;
 };
 
+let sendOsc = false
+
+const sendOSC = () => {
+  if(sendOsc == false) {
+    console.log("barabin")
+    sendOsc = true
+    // Checkbox is checked..
+} else if(sendOsc == true) {
+  oscMsg()
+  console.log("barabang")
+  sendOsc = false
+}
+  // oscMsg()
+}
+
+datachannel.onmessage = (message, userId) =>
+  addMessage(message, userId);
+  const oscMsg = () => setInterval( () => {
+    if(sendOsc == true){
+      let msg = {
+        address: "/hello/from/oscjs",
+        args: [
+          {
+            type: "f",
+            value: Math.random()
+          },
+          {
+            type: "f",
+            value: Math.random()
+          }
+        ]
+      };
+      msg = [msg.address, msg.args[0].value, msg.args[1].value]
+      console.log("Sending message", msg /*, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort*/);
+      datachannel.send(msg);
+    }
+  }, 1000 )
+
 // Demo DOM elements
-var channelInput = document.querySelector(".demo-chat-channel-input");
-var createChannelBtn = document.querySelector(".demo-chat-create");
-var joinChannelBtn = document.querySelector(".demo-chat-join");
-var messageInput = document.querySelector(".demo-chat-message-input");
-var sendBtn = document.querySelector(".demo-chat-send");
-var messageList = document.querySelector(".demo-chat-messages");
+const channelInput = document.querySelector(".demo-chat-channel-input")
+const createChannelBtn = document.querySelector(".demo-chat-create")
+const joinChannelBtn = document.querySelector(".demo-chat-join")
+const messageInput = document.querySelector(".demo-chat-message-input")
+const sendBtn = document.querySelector(".demo-chat-send")
+const messageList = document.querySelector(".demo-chat-messages")
+const createOSCbtn = document.querySelector('.send-osc')
 
 // Set up DOM listeners
 createChannelBtn.addEventListener("click", onCreateChannel);
 joinChannelBtn.addEventListener("click", onJoinChannel);
 sendBtn.addEventListener("click", onSendMessage);
 messageInput.addEventListener("keydown", onMessageKeyDown);
+createOSCbtn.addEventListener("click", sendOSC );
 
+// hook up the DataChannel.js events for successfully connecting via WebRTC, and for receiving messages over WebRTC:
 // Set up DataChannel handlers
-datachannel.onopen = function (userId) {
+datachannel.onopen = (userId) => {
   document.querySelector(".demo-connect").classList.add("inactive");
   document.querySelector(".demo-chat").classList.remove("inactive");
   messageInput.focus();
-};
-
-datachannel.onmessage = function (message, userId) {
-  addMessage(message, userId);
-};
+}
