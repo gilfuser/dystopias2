c6bf4f361f883d0e628a57ac73b6f0669e8190ff
Gil Fuser <gilfuser@gmail.com>
2018-07-29 15:58:05 -0300

    taking config.js out - still debugging

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-var config;
-try {
-  config = require("./config");
-} catch(e) {
-  console.log("Failed to find local config, falling back to environment variables");
-  config = {
-    app_id: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_APP_KEY,
-    secret: process.env.PUSHER_APP_SECRET,
-    cluster: process.env.PUSHER_APP_CLUSTER,
-  }
+const config = {
+  app_id: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
 }
 
 const express = require("express");
